380d03ffbebf0e379673561b80be44b288da07b7
Steven Fackler <sfackler@gmail.com>
2013-09-28 21:19:27 -0700

    Kill off Makefile
    Due to mozilla/rust#9602, the connection pool test had to be pulled into
    the main test file.

--- src/postgres/test.rs
+++ src/postgres/test.rs
@@ -1,6 +1,8 @@
 extern mod extra;
 extern mod postgres;
 
+use extra::comm::DuplexStream;
+use extra::future::Future;
 use extra::time;
 use extra::time::Timespec;
 use extra::json;
@@ -19,6 +21,34 @@ use postgres::{PostgresNoticeHandler,
                ResultDescription};
 use postgres::error::hack::{SyntaxError, InvalidPassword};
 use postgres::types::{ToSql, FromSql, PgInt4, PgVarchar};
+use postgres::pool::PostgresConnectionPool;
+
+
+#[test]
+// TODO move back to pool/test.rs once mozilla/rust#9602 is fixed
+// Make sure we can take both connections at once and can still get one after
+fn test_pool() {
+    let pool = PostgresConnectionPool::new("postgres://postgres@localhost", 2);
+
+    let (stream1, stream2) = DuplexStream::<(), ()>();
+
+    let mut fut1 = do Future::spawn_with(pool.clone()) |pool| {
+        let _conn = pool.get_connection();
+        stream1.send(());
+        stream1.recv();
+    };
+
+    let mut fut2 = do Future::spawn_with(pool.clone()) |pool| {
+        let _conn = pool.get_connection();
+        stream2.send(());
+        stream2.recv();
+    };
+
+    fut1.get();
+    fut2.get();
+
+    pool.get_connection();
+}
 
 #[test]
 fn test_prepare_err() {
