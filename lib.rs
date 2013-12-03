d988fc2482369cd909b53be276520fbc794ba375
Steven Fackler <sfackler@gmail.com>
2013-12-02 23:04:44 -0800

    Remove useless assignment

--- lib.rs
+++ lib.rs
@@ -515,12 +515,10 @@ impl InnerPostgresConnection {
 
         let result_desc = match self.read_message() {
             RowDescription { descriptions } => {
-                let res: ~[ResultDescription] = descriptions
-                    .move_iter().map(|desc| {
+                descriptions.move_iter().map(|desc| {
                         ResultDescription::from_row_description_entry(desc)
-                    }).collect();
-                res
-            },
+                    }).collect()
+            }
             NoData => ~[],
             _ => unreachable!()
         };
