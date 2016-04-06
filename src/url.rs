2b0460acb480320c6373653574d9e055f75a64b0
Steven Fackler <sfackler@gmail.com>
2016-04-05 22:18:14 -0700

    Style

--- src/url.rs
+++ src/url.rs
@@ -132,8 +132,7 @@ fn decode_inner(c: &str, full_url: bool) -> DecodeResult<String> {
                         };
 
                         // Only decode some characters if full_url:
-                        match Vec::<u8>::from_hex(&bytes)
-                                  .unwrap()[0] as char {
+                        match Vec::<u8>::from_hex(&bytes).unwrap()[0] as char {
                             // gen-delims:
                             ':' |
                             '/' |
