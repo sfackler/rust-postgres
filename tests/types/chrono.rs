09ede70a74f19b3dbc8ef964def73309415bda0c
Alex Gulyás <gulyas.alex@gmail.com>
2016-06-25 01:03:20 +0200

    Add date and timestamp wrappers that can represent infinity
    This patch adds the `types::Date` and `types::Timestamp` enums, which
    can represent either a date/timestamp, or positive or negative
    infinity.

--- tests/types/chrono.rs
+++ tests/types/chrono.rs
@@ -3,6 +3,8 @@ extern crate chrono;
 use self::chrono::{TimeZone, NaiveDate, NaiveTime, NaiveDateTime, DateTime, UTC};
 use types::test_type;
 
+use postgres::types::{Date, Timestamp};
+
 #[test]
 fn test_naive_date_time_params() {
     fn make_check<'a>(time: &'a str) -> (Option<NaiveDateTime>, &'a str) {
@@ -13,6 +15,20 @@ fn test_naive_date_time_params() {
                make_check("'1965-09-25 11:19:33.100314000'"),
                make_check("'2010-02-09 23:11:45.120200000'"),
                (None, "NULL")]);
+}
+
+#[test]
+fn test_with_special_naive_date_time_params() {
+    fn make_check<'a>(time: &'a str) -> (Timestamp<NaiveDateTime>, &'a str) {
+        (Timestamp::Value(NaiveDateTime::parse_from_str(time, "'%Y-%m-%d %H:%M:%S.%f'").unwrap()),
+        time)
+    }
+    test_type("TIMESTAMP",
+              &[make_check("'1970-01-01 00:00:00.010000000'"),
+               make_check("'1965-09-25 11:19:33.100314000'"),
+               make_check("'2010-02-09 23:11:45.120200000'"),
+               (Timestamp::PosInfinity, "'infinity'"),
+               (Timestamp::NegInfinity, "'-infinity'")]);
 }
 
 #[test]
@@ -28,6 +44,19 @@ fn test_date_time_params() {
 }
 
 #[test]
+fn test_with_special_date_time_params() {
+    fn make_check<'a>(time: &'a str) -> (Timestamp<DateTime<UTC>>, &'a str) {
+        (Timestamp::Value(UTC.datetime_from_str(time, "'%Y-%m-%d %H:%M:%S.%f'").unwrap()), time)
+    }
+    test_type("TIMESTAMP WITH TIME ZONE",
+              &[make_check("'1970-01-01 00:00:00.010000000'"),
+               make_check("'1965-09-25 11:19:33.100314000'"),
+               make_check("'2010-02-09 23:11:45.120200000'"),
+               (Timestamp::PosInfinity, "'infinity'"),
+               (Timestamp::NegInfinity, "'-infinity'")]);
+}
+
+#[test]
 fn test_date_params() {
     fn make_check<'a>(time: &'a str) -> (Option<NaiveDate>, &'a str) {
         (Some(NaiveDate::parse_from_str(time, "'%Y-%m-%d'").unwrap()), time)
@@ -37,6 +66,19 @@ fn test_date_params() {
                make_check("'1965-09-25'"),
                make_check("'2010-02-09'"),
                (None, "NULL")]);
+}
+
+#[test]
+fn test_with_special_date_params() {
+    fn make_check<'a>(date: &'a str) -> (Date<NaiveDate>, &'a str) {
+        (Date::Value(NaiveDate::parse_from_str(date, "'%Y-%m-%d'").unwrap()), date)
+    }
+    test_type("DATE",
+              &[make_check("'1970-01-01'"),
+               make_check("'1965-09-25'"),
+               make_check("'2010-02-09'"),
+               (Date::PosInfinity, "'infinity'"),
+               (Date::NegInfinity, "'-infinity'")]);
 }
 
 #[test]
