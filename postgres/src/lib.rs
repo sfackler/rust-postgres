bd2710b9c9527c805bdbd48d56e0597d4a563883
Steven Fackler <sfackler@gmail.com>
2017-02-04 15:36:15 -0800

    Release postgres v0.13.6

--- postgres/src/lib.rs
+++ postgres/src/lib.rs
@@ -66,7 +66,7 @@
 //!         .unwrap();
 //! }
 //! ```
-#![doc(html_root_url="https://docs.rs/postgres/0.13.5")]
+#![doc(html_root_url="https://docs.rs/postgres/0.13.6")]
 #![warn(missing_docs)]
 #![allow(unknown_lints, needless_lifetimes, doc_markdown)] // for clippy
 
