afce0b7d144d8eb0cfccbff5761c973635e473d0
Sam Rijs <srijs@airpost.net>
2018-09-25 21:30:51 +1000

    Add Json wrapper type

--- postgres-shared/src/types/serde_json.rs
+++ postgres-shared/src/types/serde_json.rs
@@ -1,13 +1,22 @@
+extern crate serde;
 extern crate serde_json;
 
+use self::serde::{Deserialize, Serialize};
 use self::serde_json::Value;
 use std::error::Error;
-use std::io::{Read, Write};
+use std::fmt::Debug;
+use std::io::Read;
 
 use types::{FromSql, IsNull, ToSql, Type};
 
-impl<'a> FromSql<'a> for Value {
-    fn from_sql(ty: &Type, mut raw: &[u8]) -> Result<Value, Box<Error + Sync + Send>> {
+#[derive(Debug)]
+pub struct Json<T>(pub T);
+
+impl<'a, T> FromSql<'a> for Json<T>
+where
+    T: Deserialize<'a>,
+{
+    fn from_sql(ty: &Type, mut raw: &'a [u8]) -> Result<Json<T>, Box<Error + Sync + Send>> {
         if *ty == Type::JSONB {
             let mut b = [0; 1];
             raw.read_exact(&mut b)?;
@@ -16,7 +25,33 @@ impl<'a> FromSql<'a> for Value {
                 return Err("unsupported JSONB encoding version".into());
             }
         }
-        serde_json::de::from_reader(raw).map_err(Into::into)
+        serde_json::de::from_slice(raw)
+            .map(Json)
+            .map_err(Into::into)
+    }
+
+    accepts!(JSON, JSONB);
+}
+
+impl<T> ToSql for Json<T>
+where
+    T: Serialize + Debug,
+{
+    fn to_sql(&self, ty: &Type, out: &mut Vec<u8>) -> Result<IsNull, Box<Error + Sync + Send>> {
+        if *ty == Type::JSONB {
+            out.push(1);
+        }
+        serde_json::ser::to_writer(out, &self.0)?;
+        Ok(IsNull::No)
+    }
+
+    accepts!(JSON, JSONB);
+    to_sql_checked!();
+}
+
+impl<'a> FromSql<'a> for Value {
+    fn from_sql(ty: &Type, raw: &[u8]) -> Result<Value, Box<Error + Sync + Send>> {
+        Json::<Value>::from_sql(ty, raw).map(|json| json.0)
     }
 
     accepts!(JSON, JSONB);
@@ -24,11 +59,7 @@ impl<'a> FromSql<'a> for Value {
 
 impl ToSql for Value {
     fn to_sql(&self, ty: &Type, out: &mut Vec<u8>) -> Result<IsNull, Box<Error + Sync + Send>> {
-        if *ty == Type::JSONB {
-            out.push(1);
-        }
-        write!(out, "{}", self)?;
-        Ok(IsNull::No)
+        Json(self).to_sql(ty, out)
     }
 
     accepts!(JSON, JSONB);
