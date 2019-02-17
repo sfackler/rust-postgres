fb6e19ae6093958ca5e338a7bc9c7f0e841bc664
Steven Fackler <sfackler@gmail.com>
2019-02-17 12:33:52 -0800

    Use geo-types rather than geo
    Closes #418

--- tokio-postgres/src/types/geo_types_04.rs
+++ tokio-postgres/src/types/geo_types_04.rs
@@ -1,5 +1,5 @@
 use fallible_iterator::FallibleIterator;
-use geo_011::{Coordinate, LineString, Point, Rect};
+use geo_types_04::{Coordinate, LineString, Point, Rect};
 use postgres_protocol::types;
 use std::error::Error;
 
--- tokio-postgres/src/types/mod.rs
+++ tokio-postgres/src/types/mod.rs
@@ -78,8 +78,8 @@ mod bit_vec_05;
 mod chrono_04;
 #[cfg(feature = "with-eui48-0.4")]
 mod eui48_04;
-#[cfg(feature = "with-geo-0.11")]
-mod geo_011;
+#[cfg(feature = "with-geo-types-0.4")]
+mod geo_types_04;
 #[cfg(feature = "with-serde_json-1")]
 mod serde_json_1;
 #[cfg(feature = "with-uuid-0.7")]
