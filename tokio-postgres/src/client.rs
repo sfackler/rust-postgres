b390675f3b834a4141774d78e75048bb8b120f67
John C. Doneth <doneth7@gmail.com>
2019-07-23 16:23:40 -0400

    working on client::prepare

--- tokio-postgres/src/client.rs
+++ tokio-postgres/src/client.rs
@@ -1,10 +1,14 @@
+use crate::codec::FrontendMessage;
 use crate::connection::{Request, RequestMessages};
 use crate::error::Error;
 use crate::responses::{self, Responses};
 use crate::statement::Statement;
 use crate::types::Type;
-
+use fallible_iterator::FallibleIterator;
 use futures::channel::mpsc;
+use futures::StreamExt;
+use postgres_protocol::message::backend::Message;
+use postgres_protocol::message::frontend;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 fn next_statement() -> String {
@@ -50,8 +54,80 @@ impl Client {
         query: &str,
         param_types: &[Type],
     ) -> Result<Statement, Error> {
-        unimplemented!();
-        //self.sender.unbounded_send(PendingRequest)
+        let name = next_statement();
+        let pending = self.pending(|buf| {
+            frontend::parse(&name, query, param_types.iter().map(Type::oid), buf)
+                .map_err(Error::parse)?;
+            frontend::describe(b'S', &name, buf).map_err(Error::parse)?;
+            frontend::sync(buf);
+            Ok(())
+        });
+
+        let mut response = self.send(pending)?;
+
+        let message = response.next().await;
+
+        match message {
+            Some(Ok(Message::ParseComplete)) => (),
+            Some(Ok(Message::ErrorResponse(body))) => return Err(Error::db(body)),
+            Some(Err(e)) => return Err(e),
+            Some(_) => return Err(Error::unexpected_message()),
+            None => return Err(Error::closed()),
+        };
+
+        let message = response.next().await;
+
+        let parameters: Vec<_> = match message {
+            Some(Ok(Message::ParameterDescription(body))) => {
+                body.parameters().collect().map_err(Error::parse)?
+            }
+            Some(Err(e)) => return Err(e),
+            Some(_) => return Err(Error::unexpected_message()),
+            None => return Err(Error::closed()),
+        };
+
+        let message = response.next().await;
+
+        let columns = match message {
+            Some(Ok(Message::RowDescription(body))) => body
+                .fields()
+                .map(|f| Ok((f.name().to_string(), f.type_oid())))
+                .collect()
+                .map_err(Error::parse)?,
+            Some(Ok(Message::NoData)) => vec![],
+            Some(_) => return Err(Error::unexpected_message()),
+            None => return Err(Error::closed()),
+        };
+
+        let mut parameters = parameters.into_iter();
+        if let Some(oid) = parameters.next() {
+            // fetch params
+            unimplemented!("not yet finished");
+        }
+
+        let mut columns = columns.into_iter();
+        if let Some((name, oid)) = columns.next() {
+            // fetch columns
+            unimplemented!("not yet finished");
+        }
+
+        unimplemented!("not yet finished");
+
+        /*Ok(Statement::new(
+            name,
+            parameters,
+            columns,
+        ))*/
+    }
+
+    pub(crate) fn pending<F>(&self, messages: F) -> PendingRequest
+    where
+        F: FnOnce(&mut Vec<u8>) -> Result<(), Error>,
+    {
+        let mut buf = vec![];
+        PendingRequest(
+            messages(&mut buf).map(|()| RequestMessages::Single(FrontendMessage::Raw(buf))),
+        )
     }
 
     pub(crate) fn send(&self, request: PendingRequest) -> Result<Responses, Error> {
