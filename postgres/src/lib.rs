a1efddd64a47aca595d5801a6ae40c51832eb0cf
Naim A <227396+naim94a@users.noreply.github.com>
2020-05-12 13:35:10 +0300

    fix typo

--- postgres/src/lib.rs
+++ postgres/src/lib.rs
@@ -57,7 +57,7 @@
 //! | `with-chrono-0_4` | Enable support for the `chrono` crate. | [chrono](https://crates.io/crates/chrono) 0.4 | no |
 //! | `with-eui48-0_4` | Enable support for the `eui48` crate. | [eui48](https://crates.io/crates/eui48) 0.4 | no |
 //! | `with-geo-types-0_4` | Enable support for the `geo-types` crate. | [geo-types](https://crates.io/crates/geo-types) 0.4 | no |
-//! | `with-serde_json-1` | Enable support for the `serde_json` crate. | [eui48](https://crates.io/crates/serde_json) 1.0 | no |
+//! | `with-serde_json-1` | Enable support for the `serde_json` crate. | [serde_json](https://crates.io/crates/serde_json) 1.0 | no |
 //! | `with-uuid-0_8` | Enable support for the `uuid` crate. | [uuid](https://crates.io/crates/uuid) 0.8 | no |
 //! | `with-time-0_2` | Enable support for the `time` crate. | [time](https://crates.io/crates/time) 0.2 | no |
 #![doc(html_root_url = "https://docs.rs/postgres/0.17")]
