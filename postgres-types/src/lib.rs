08b7c65f8d367a701054327918ca653d8a21d66e
Ruslan Talpa <ruslan.talpa@gmail.com>
2021-11-02 11:13:25 +0200

    remove as_string

--- postgres-types/src/lib.rs
+++ postgres-types/src/lib.rs
@@ -772,17 +772,12 @@ pub trait ToSql: fmt::Debug {
 
     /// Specify the encode format
     fn encode_format(&self) -> i16 { 1 }
-
-    /// return string representation
-    fn as_string(&self) -> String {
-        panic!("as_string not implemented for {:?}", self)
-    }
 }
 
 
 /// A Wrapper type used for sending query parameters encoded as unknown.
 #[derive(Debug)]
-pub struct Unknown<'a>(pub &'a (dyn ToSql + Sync));
+pub struct Unknown<'a>(pub &'a str);
 
 impl ToSql for Unknown<'_> {
     fn to_sql(
@@ -792,7 +787,7 @@ impl ToSql for Unknown<'_> {
     ) -> Result<IsNull, Box<dyn Error + Sync + Send>> {
         match *self {
             Unknown(val) => {
-                types::text_to_sql(&val.as_string(), out);
+                types::text_to_sql(val, out);
                 Ok(IsNull::No)
             }
         }
@@ -939,7 +934,7 @@ impl<'a> ToSql for &'a str {
             _ => false,
         }
     }
-    fn as_string(&self) -> String { self.to_string() }
+
     to_sql_checked!();
 }
 
@@ -963,7 +958,7 @@ impl ToSql for String {
     fn accepts(ty: &Type) -> bool {
         <&str as ToSql>::accepts(ty)
     }
-    fn as_string(&self) -> String { self.clone() }
+
     to_sql_checked!();
 }
 
@@ -976,10 +971,6 @@ macro_rules! simple_to {
                       -> Result<IsNull, Box<dyn Error + Sync + Send>> {
                 types::$f(*self, w);
                 Ok(IsNull::No)
-            }
-
-            fn as_string(&self) -> String {
-                format!("{}", &self)
             }
 
             accepts!($($expected),+);
