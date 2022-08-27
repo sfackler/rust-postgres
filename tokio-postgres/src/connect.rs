3c9315e3200f5eb99bb5a9b5998aca555951d691
Trung Dinh <dinhanhtrung@gmail.com>
2022-08-27 11:40:57 -0700

    IpAddr + try hostaddr first

--- tokio-postgres/src/connect.rs
+++ tokio-postgres/src/connect.rs
@@ -5,8 +5,8 @@ use crate::connect_socket::connect_socket;
 use crate::tls::{MakeTlsConnect, TlsConnect};
 use crate::{Client, Config, Connection, Error, SimpleQueryMessage, Socket};
 use futures_util::{future, pin_mut, Future, FutureExt, Stream};
-use std::io;
 use std::task::Poll;
+use std::{cmp, io};
 
 pub async fn connect<T>(
     mut tls: T,
@@ -15,25 +15,35 @@ pub async fn connect<T>(
 where
     T: MakeTlsConnect<Socket>,
 {
-    if config.host.is_empty() {
-        return Err(Error::config("host missing".into()));
+    if config.host.is_empty() && config.hostaddr.is_empty() {
+        return Err(Error::config("both host and hostaddr are missing".into()));
     }
 
-    if config.port.len() > 1 && config.port.len() != config.host.len() {
-        return Err(Error::config("invalid number of ports".into()));
-    }
-
-    if !config.hostaddr.is_empty() && config.hostaddr.len() != config.host.len() {
+    if !config.host.is_empty()
+        && !config.hostaddr.is_empty()
+        && config.host.len() != config.hostaddr.len()
+    {
         let msg = format!(
-            "invalid number of hostaddrs ({}). Possible values: 0 or number of hosts ({})",
-            config.hostaddr.len(),
+            "number of hosts ({}) is different from number of hostaddrs ({})",
             config.host.len(),
+            config.hostaddr.len(),
         );
         return Err(Error::config(msg.into()));
     }
 
+    // At this point, either one of the following two scenarios could happen:
+    // (1) either config.host or config.hostaddr must be empty;
+    // (2) if both config.host and config.hostaddr are NOT empty; their lengths must be equal.
+    let num_hosts = cmp::max(config.host.len(), config.hostaddr.len());
+
+    if config.port.len() > 1 && config.port.len() != num_hosts {
+        return Err(Error::config("invalid number of ports".into()));
+    }
+
     let mut error = None;
-    for (i, host) in config.host.iter().enumerate() {
+    for i in 0..num_hosts {
+        let host = config.host.get(i);
+        let hostaddr = config.hostaddr.get(i);
         let port = config
             .port
             .get(i)
@@ -42,27 +52,30 @@ where
             .unwrap_or(5432);
 
         // The value of host is always used as the hostname for TLS validation.
+        // postgres doesn't support TLS over unix sockets, so the choice for Host::Unix variant here doesn't matter
         let hostname = match host {
-            Host::Tcp(host) => host.as_str(),
-            // postgres doesn't support TLS over unix sockets, so the choice here doesn't matter
-            #[cfg(unix)]
-            Host::Unix(_) => "",
+            Some(Host::Tcp(host)) => host.as_str(),
+            _ => "",
         };
         let tls = tls
             .make_tls_connect(hostname)
             .map_err(|e| Error::tls(e.into()))?;
 
-        // If both host and hostaddr are specified, the value of hostaddr is used to to establish the TCP connection.
-        let hostaddr = match host {
-            Host::Tcp(_hostname) => match config.hostaddr.get(i) {
-                Some(hostaddr) if hostaddr.is_empty() => Host::Tcp(hostaddr.clone()),
-                _ => host.clone(),
-            },
-            #[cfg(unix)]
-            Host::Unix(_v) => host.clone(),
+        // Try to use the value of hostaddr to establish the TCP connection,
+        // fallback to host if hostaddr is not present.
+        let addr = match hostaddr {
+            Some(ipaddr) => Host::Tcp(ipaddr.to_string()),
+            None => {
+                if let Some(host) = host {
+                    host.clone()
+                } else {
+                    // This is unreachable.
+                    return Err(Error::config("both host and hostaddr are empty".into()));
+                }
+            }
         };
 
-        match connect_once(&hostaddr, port, tls, config).await {
+        match connect_once(&addr, port, tls, config).await {
             Ok((client, connection)) => return Ok((client, connection)),
             Err(e) => error = Some(e),
         }
