23f27af737010458c1de8ebeb0582945d1241038
candywater <candywater@users.noreply.github.com>
2022-12-24 11:13:53 +0800

    should add main() to newbot in rust

--- postgres/src/lib.rs
+++ postgres/src/lib.rs
@@ -5,33 +5,33 @@
 //! ```no_run
 //! use postgres::{Client, NoTls};
 //!
-//! # fn main() -> Result<(), postgres::Error> {
-//! let mut client = Client::connect("host=localhost user=postgres", NoTls)?;
+//! fn main() -> Result<(), postgres::Error> {
+//!     let mut client = Client::connect("host=localhost user=postgres", NoTls)?;
 //!
-//! client.batch_execute("
-//!     CREATE TABLE person (
-//!         id      SERIAL PRIMARY KEY,
-//!         name    TEXT NOT NULL,
-//!         data    BYTEA
-//!     )
-//! ")?;
+//!     client.batch_execute("
+//!         CREATE TABLE person (
+//!             id      SERIAL PRIMARY KEY,
+//!             name    TEXT NOT NULL,
+//!             data    BYTEA
+//!         )
+//!     ")?;
 //!
-//! let name = "Ferris";
-//! let data = None::<&[u8]>;
-//! client.execute(
-//!     "INSERT INTO person (name, data) VALUES ($1, $2)",
-//!     &[&name, &data],
-//! )?;
+//!     let name = "Ferris";
+//!     let data = None::<&[u8]>;
+//!     client.execute(
+//!         "INSERT INTO person (name, data) VALUES ($1, $2)",
+//!         &[&name, &data],
+//!     )?;
 //!
-//! for row in client.query("SELECT id, name, data FROM person", &[])? {
-//!     let id: i32 = row.get(0);
-//!     let name: &str = row.get(1);
-//!     let data: Option<&[u8]> = row.get(2);
+//!     for row in client.query("SELECT id, name, data FROM person", &[])? {
+//!         let id: i32 = row.get(0);
+//!         let name: &str = row.get(1);
+//!         let data: Option<&[u8]> = row.get(2);
 //!
-//!     println!("found person: {} {} {:?}", id, name, data);
+//!         println!("found person: {} {} {:?}", id, name, data);
+//!     }
+//!     Ok(())
 //! }
-//! # Ok(())
-//! # }
 //! ```
 //!
 //! # Implementation
