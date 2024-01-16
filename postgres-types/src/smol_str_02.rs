0c249267b8e4d610b03922a9e80ceb10fa1760f8
novacrazy <novacrazy@gmail.com>
2024-01-16 13:43:00 -0600

    Add smol_str 0.2 support

---
+++ postgres-types/src/smol_str_02.rs
@@ -0,0 +1,27 @@
+use bytes::BytesMut;
+use smol_str_02::SmolStr;
+use std::error::Error;
+
+use crate::{FromSql, IsNull, ToSql, Type};
+
+impl<'a> FromSql<'a> for SmolStr {
+    fn from_sql(ty: &Type, raw: &'a [u8]) -> Result<Self, Box<dyn Error + Sync + Send>> {
+        Ok(SmolStr::new(<&str as FromSql>::from_sql(ty, raw)?))
+    }
+
+    fn accepts(ty: &Type) -> bool {
+        <&str as FromSql>::accepts(ty)
+    }
+}
+
+impl ToSql for SmolStr {
+    fn to_sql(&self, ty: &Type, out: &mut BytesMut) -> Result<IsNull, Box<dyn Error + Sync + Send>> {
+        <&str as ToSql>::to_sql(&self.as_str(), ty, out)
+    }
+
+    fn accepts(ty: &Type) -> bool {
+        <&str as ToSql>::accepts(ty)
+    }
+
+    to_sql_checked!();
+}
