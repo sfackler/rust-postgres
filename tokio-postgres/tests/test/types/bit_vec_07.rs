a23036e387e6a6db905202ae374b54df5788f193
crusopaul <crusopaul@gmail.com>
2025-02-21 20:19:19 -0500

    Add support for bit-vec 0.7 & 0.8

---
+++ tokio-postgres/tests/test/types/bit_vec_07.rs
@@ -0,0 +1,31 @@
+use bit_vec_07::BitVec;
+
+use crate::types::test_type;
+
+#[tokio::test]
+async fn test_bit_params() {
+    let mut bv = BitVec::from_bytes(&[0b0110_1001, 0b0000_0111]);
+    bv.pop();
+    bv.pop();
+    test_type(
+        "BIT(14)",
+        &[(Some(bv), "B'01101001000001'"), (None, "NULL")],
+    )
+    .await
+}
+
+#[tokio::test]
+async fn test_varbit_params() {
+    let mut bv = BitVec::from_bytes(&[0b0110_1001, 0b0000_0111]);
+    bv.pop();
+    bv.pop();
+    test_type(
+        "VARBIT",
+        &[
+            (Some(bv), "B'01101001000001'"),
+            (Some(BitVec::from_bytes(&[])), "B''"),
+            (None, "NULL"),
+        ],
+    )
+    .await
+}
